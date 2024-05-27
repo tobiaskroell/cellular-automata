0d8be8904256cacba83fb96852afb90cf630b3ae
Tobias Kroell <kroelltob@gmail.com>
2024-05-27 21:10:22 +0200

    Enable anti-aliasing for smoother lines

--- CellularAutomata/src/Main.java
+++ CellularAutomata/src/Main.java
@@ -21,7 +21,9 @@ public class Main extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g); // Ensures the panel is properly rendered
         Graphics2D cnv = (Graphics2D) g; // Cast to Graphics2D for more advanced control
-        
+
+        // Enable anti-aliasing for smoother lines
+        cnv.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         cnv.setColor(Color.BLACK); // Set drawing color
 
         int[] cells = {1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0};
