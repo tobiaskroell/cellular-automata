b17a8cf1f1ccafba80994bcf994114fae5440530
Tobias Kroell <kroelltob@gmail.com>
2024-05-27 21:09:36 +0200

    Create Canvas and draw rectangles

---
+++ CellularAutomata/src/Main.java
@@ -0,0 +1,58 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JPanel {
+    public static void main(String[] args) {
+        createCanvas();
+
+    }
+
+    private static void createCanvas() {
+        JFrame frame = new JFrame("Cellular Automata");
+        Main main = new Main();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 400);
+        frame.add(main);
+        frame.setVisible(true);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g); // Ensures the panel is properly rendered
+        Graphics2D cnv = (Graphics2D) g; // Cast to Graphics2D for more advanced control
+        
+        cnv.setColor(Color.BLACK); // Set drawing color
+
+        int[] cells = {1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0};
+        int x = 0;
+        int y = 0;
+        int cellWidth = 40;
+        int cellHeight = 40;
+        int cellsInRow = 0;
+
+        for (int i = 0; i < cells.length; i++) {
+            if (cellsInRow == 10) {
+                x = 0;
+                y += cellHeight;
+                cellsInRow = 0;
+            }
+
+            if (cells[i] == 1) {
+                cnv.fillRect(x, y, cellWidth, cellHeight);
+            } else {
+                cnv.drawRect(x, y, cellWidth, cellHeight);
+            }
+            x += cellWidth;
+            cellsInRow++;
+        }
+    }
+}
+
+
+
+
+
+
+
+
